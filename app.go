a21b2eebccfc7aa523876c9f0ccd8a0990769549
Charles Fenwick Elliott <charles@fenwickelliott.io>
2018-05-02 12:01:45 -0400

    Docker

--- app.go
+++ app.go
@@ -10,19 +10,16 @@ import (
 func main() {
 	port := os.Getenv("PORT")
 	if port == "" {
-		port = "8181"
+		port = "80"
 	}
 
+	fmt.Println("Setting up, listening on (internal) port:", port)
+
 	http.HandleFunc("/", root)
-	http.HandleFunc("/echo", echo)
 	http.ListenAndServe(":"+port, nil)
 }
 
 func root(w http.ResponseWriter, r *http.Request) {
 	fmt.Println("root got")
-	io.WriteString(w, "Root directory")
-}
-
-func echo(w http.ResponseWriter, r *http.Request) {
-	fmt.Println(r.URL.Query())
+	io.WriteString(w, "Root")
 }
